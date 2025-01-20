bc4a0f1f898612536cb42b0327ceec6e89dd8b79
Maficks <berrrserk2020@mail.ru>
2025-01-20 11:57:04 +0500

    first steps

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,23 @@
 <script setup>
+import { ref } from 'vue';
 
+const mouseX = ref(0);
+const mouseY = ref(0);
+
+// Функция для отслеживания положения мыши
+const handleMouseMove = (event) => {
+  mouseX.value = event.clientX;
+  mouseY.value = event.clientY;
+};
 </script>
 
 <template>
-  <div>
+  <div class="root">
+    <div class="background-image"></div>
+    <div class="background"></div>
+    <div class="content">
 
+    </div>
   </div>
 </template>
 
