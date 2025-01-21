42067199fa5a159a8e4c37f13cd4f64ad35215a5
Maficks <berrrserk2020@mail.ru>
2025-01-21 11:51:20 +0500

    improving

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, onMounted } from 'vue';
+import {ref, onMounted, watch} from 'vue';
 
 const mouseX = ref(0);
 const mouseY = ref(0);
@@ -11,6 +11,11 @@ const handleMouseMove = (event) => {
 
 onMounted(() => {
   document.addEventListener('mousemove', handleMouseMove);
+});
+
+watch([mouseX, mouseY], () => {
+  const xPos = (mouseX.value / window.innerWidth) * 100;
+  const yPos = (mouseY.value / window.innerHeight) * 100;
 });
 </script>
 
@@ -35,5 +40,14 @@ onMounted(() => {
   justify-content: center;
   align-items: center;
   flex-direction: column;
+}
+.background-image {
+  background: url("assets/1.jpeg") round;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  opacity: 30%;
 }
 </style>
